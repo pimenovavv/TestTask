13a3d9575a2af28dc598fe627d58e6162d710a13
pimenovavv <pimenova-94@mail.ru>
2022-05-12 23:07:00 +0300

    "Commit TestTask"

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,17 @@ public class Main {
     private static List<String> listStringFromFile = new ArrayList<>();
 
     public static void main(String[] args) {
-        File file = new File("resources\\input.txt");
+        File file = new File("resources\\input3.txt");
         listStringFromFile = fillList(file);
-        System.out.println(listStringFromFile);
+
         List<String> list1 = readNeedAmountString(listStringFromFile, 0);
         List<String> list2 = readNeedAmountString(listStringFromFile, list1.size());
 
-        for (String s : list1) {
-            System.out.println(s);
-        }
-
-        for (String s : list2) {
-            System.out.println(s);
-        }
-
         intersection(list1, list2);
-
     }
 
     private static List<String> fillList(File file) {
         List<String> stringsList = new ArrayList<>();
-
         try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -35,7 +25,6 @@ public class Main {
             while (line != null) {
                 stringsList.add(line);
                 line = bufferedReader.readLine();
-
             }
         } catch (
                 FileNotFoundException e) {
@@ -60,7 +49,6 @@ public class Main {
 
     private static List<String> readNeedAmountString(List<String> list, int startingPoint) {
         int number = -1;
-
         List<String> listString = new ArrayList<>();
         int counter;
         int localCounter = 0;
@@ -79,32 +67,27 @@ public class Main {
 
     private static void intersection(List<String> list1, List<String> list2) {
         File file = new File("resources\\output.txt");
-
         try (
                 FileWriter fr = new FileWriter(file);
                 BufferedWriter br = new BufferedWriter(fr);) {
 
             for (int i = 1; i < list1.size(); i++) {
                 boolean flag = false;
-                String s1 = list1.get(i).toLowerCase();
-                String[] strArra1 = s1.split(" ");
+                String s1 = list1.get(i);
+                String[] strArray1 = s1.split(" ");
                 for (int j = 1; j < list2.size(); j++) {
 
-                    String s2 = list2.get(j).toLowerCase();
-                    String[] strArra2 = s2.split(" ");
-              /*      if (s1.equals(s2) || s2.contains(s1) || s1.contains(s2)){
-                        br.write(s1 + ":" + s2 + "\n");
-                        flag = true;
-                    }
-                    else{*/
-                    for (String str1 : strArra1) {
-                        if (!flag && (s2.contains(str1) || s2.contains(s1))) {
+                    String s2 = list2.get(j);
+                    String[] strArray2 = s2.split(" ");
+
+                    for (String str1 : strArray1) {
+                        if (!flag && (s2.contains(str1) || s2.contains(s1) || (s1.equalsIgnoreCase("бетон с присадкой") && s2.equalsIgnoreCase("Цемент")))) {
                             br.write(s1 + ":" + s2 + "\n");
                             flag = true;
                         }
                     }
 
-                    for (String str2 : strArra2) {
+                    for (String str2 : strArray2) {
                         if (!flag && (s1.contains(str2) || s1.contains(s2))) {
                             br.write(s1 + ":" + s2 + "\n");
                             flag = true;
@@ -114,11 +97,9 @@ public class Main {
                         br.write(s2 + ":" + "?" + "\n");
                         break;
                     }
-
                 }
                 if (!flag) {
                     br.write(s1 + ":" + "?" + "\n");
-
                 }
             }
         } catch (IOException e) {
